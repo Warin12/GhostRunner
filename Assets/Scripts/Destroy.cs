5f7f3e06e89f61c879b48b51ae0c97f7fab80b6c
Warin12 <evanw8@learn.cssd.ab.ca>
2019-06-18 14:14:01 -0600

    first commit

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public float life;
+
+    public void Start()
+    {
+        Destroy(gameObject, life);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    [SerializeField] public float Ymove;
+    [SerializeField] public float speed;
+    [SerializeField] public float maxHeight;
+    [SerializeField] public float minHeight;
+    [SerializeField] public int health = 3;
+
+    public Text healthDisplay;
+
+    public GameObject gameOver;
+
+
+    private void Update()
+    {
+        healthDisplay.text = health.ToString();
+        
+        if (health <= 0)
+        {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+         
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Ymove);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Ymove);
+        }
+    }
+}
